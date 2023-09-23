8005fa274e1f2cff6d1efc65555d04dffe17ddaa
Alexey Lozhkin <leha.lozhkin.02@mail.ru>
2023-09-23 11:22:58 +0300

    replace all games to Games

--- src/CalcGame.php
+++ src/CalcGame.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace BrainGames\CalcGame;
+
 use function cli\line;
 use function BrainGames\Engine\answerTheQuestion;
 use function BrainGames\Engine\askQuestion;
@@ -23,37 +24,4 @@ function getRandomOperation(): string
     $quantityOperations = count($operations);
 
     return $operations[rand(0, $quantityOperations - 1)];
-}
-
-function startCalcGame(): int
-{
-    $quantityCorrectAnswers = 0;
-    $isCorrectAnswer = true;
-    
-
-    do {
-        $firstOperand = getRandomNumber();
-        $operation = getRandomOperation();
-
-        if ($operation === '*') {
-            $secondOperand = getRandomNumber(0, 10);
-        } else {
-            $secondOperand = getRandomNumber();
-        }
-
-        $expression = implode(" ", [$firstOperand, $operation, $secondOperand]);
-        askQuestion($expression);
-        $answer = answerTheQuestion();
-        $expected = calcExpression($firstOperand, $secondOperand, $operation);
-        $isCorrectAnswer = checkAnswer($expected, $answer);
-
-        if ($isCorrectAnswer) {
-            $quantityCorrectAnswers++;
-            line('Correct!');
-        } else {
-            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $expected);
-        }
-    } while (isContinueGame($quantityCorrectAnswers, $isCorrectAnswer));
-
-    return $quantityCorrectAnswers;
 }
--- src/IsEvenGame.php
+++ src/IsEvenGame.php
@@ -9,29 +9,6 @@ use function BrainGames\Engine\checkAnswer;
 use function BrainGames\Engine\getRandomNumber;
 use function BrainGames\Engine\isContinueGame;
 
-function startIsEvenGame(): int
-{
-    $quantityCorrectAnswers = 0;
-    $isCorrectAnswer = true;
-
-    do {
-        $number = getRandomNumber();
-        askQuestion($number);
-        $answer = answerTheQuestion();
-        $expected = isEven($number) ? 'yes' : 'no';
-        $isCorrectAnswer = checkAnswer($expected, $answer);
-
-        if ($isCorrectAnswer) {
-            $quantityCorrectAnswers++;
-            line('Correct!');
-        } else {
-            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $expected);
-        }
-    } while (isContinueGame($quantityCorrectAnswers, $isCorrectAnswer));
-
-    return $quantityCorrectAnswers;
-}
-
 function isEven(int $number): bool
 {
     return $number % 2 === 0 ? true : false;
