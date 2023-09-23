40892f3d0552488c661fb3da3e8e4180c137bf62
Alexey Lozhkin <leha.lozhkin.02@mail.ru>
2023-09-23 14:37:20 +0300

    update FindGCDGameHelpers.php

--- src/Helpers/FindGCDGameHelpers.php
+++ src/Helpers/FindGCDGameHelpers.php
@@ -2,9 +2,9 @@
 
 namespace BrainGames\Helpers\FindGCDGame;
 
-function getGCD(int $num1, int $num2): int
+function getGCD(int $num1, int $num2, int $minGCD): int
 {
-    $gcd = 1;
+    $gcd = $minGCD;
 
     for ($i = getSmallerNumber($num1, $num2); $i > $gcd; $i--) {
         if (($num1 % $i == 0) && ($num2 % $i == 0)) {
