f02cb2970b636ed656e9d68d6145cb43fafb5a26
Alexey Lozhkin <leha.lozhkin.02@mail.ru>
2023-09-23 14:40:34 +0300

    delete all unusable dependencies

--- src/Helpers/CalcGameHelpers.php
+++ src/Helpers/CalcGameHelpers.php
@@ -2,13 +2,6 @@
 
 namespace BrainGames\Helpers\CalcGame;
 
-use function cli\line;
-use function BrainGames\Engine\answerTheQuestion;
-use function BrainGames\Engine\askQuestion;
-use function BrainGames\Engine\checkAnswer;
-use function BrainGames\Engine\getRandomNumber;
-use function BrainGames\Engine\isContinueGame;
-
 function calcExpression(int $firstOperand, int $secondOperand, string $operation): int
 {
     return match ($operation) {
--- src/Helpers/IsEvenGameHelpers.php
+++ src/Helpers/IsEvenGameHelpers.php
@@ -2,13 +2,6 @@
 
 namespace BrainGames\Helpers\IsEvenGame;
 
-use function cli\line;
-use function BrainGames\Engine\answerTheQuestion;
-use function BrainGames\Engine\askQuestion;
-use function BrainGames\Engine\checkAnswer;
-use function BrainGames\Engine\getRandomNumber;
-use function BrainGames\Engine\isContinueGame;
-
 function isEven(int $number): bool
 {
     return $number % 2 === 0 ? true : false;
