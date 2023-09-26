a798c5f68d3349b316409ae20a239e41621070c0
Alexey Lozhkin <leha.lozhkin.02@mail.ru>
2023-09-26 15:52:34 +0300

    add BrainPrimeHelpers.php

---
+++ src/Helpers/BrainPrimeHelpers.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace BrainGames\Helpers\BrainPrimeHelpers;
+
+function isPrime(int $number): bool
+{
+    $isPrime = true;
+
+    for ($i = 2; $isPrime && $i < $number; $i++) {
+        if ($number % $i === 0) {
+            $isPrime = false;
+        }
+    }
+
+    return $isPrime;
+}
