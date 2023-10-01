a320be137241b41624e3a35107fe310741010dc4
Alexey Lozhkin <leha.lozhkin.02@mail.ru>
2023-10-01 11:39:51 +0300

    update Info.php

--- src/Info.php
+++ src/Info.php
@@ -5,13 +5,6 @@ namespace BrainGames\Info;
 use function cli\line;
 use function cli\prompt;
 
-const BRAIN_EVEN_RULES = 'Answer "yes" if the number is even, otherwise answer "no".';
-const BRAIN_CALC_RULES = 'What is the result of the expression?';
-const BRAIN_GSD_RULES = 'Find the greatest common divisor of given numbers.';
-const BRAIN_PROGRESSION_RULES = 'What number is missing in the progression?';
-const BRAIN_PRIME_RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-const UNKNOWN_GAME_RULES = 'There are no rules for this game.';
-
 function welcomePlayer(): string
 {
     line("Welcome to the Brain Games!");
@@ -21,23 +14,16 @@ function welcomePlayer(): string
     return $name;
 }
 
-function showRules(string $gameName): void
+function showRules(string $rules): void
 {
-    match ($gameName) {
-        'BrainEven' => line(BRAIN_EVEN_RULES),
-        'BrainCalc' => line(BRAIN_CALC_RULES),
-        'BrainGCD' => line(BRAIN_GSD_RULES),
-        'BrainProgression' => line(BRAIN_PROGRESSION_RULES),
-        'BrainPrime' => line(BRAIN_PRIME_RULES),
-        default => line(UNKNOWN_GAME_RULES),
-    };
+    line($rules);
 }
 
-function showResult(int $quantityCorrectAnswer, string $name): void
+function showResult(bool $isWonGame, string $player): void
 {
-    if ($quantityCorrectAnswer === 3) {
-        line('Congratulations, %s!', $name);
+    if ($isWonGame) {
+        line('Congratulations, %s!', $player);
     } else {
-        line("Let's try again, %s!", $name);
+        line("Let's try again, %s!", $player);
     }
 }
