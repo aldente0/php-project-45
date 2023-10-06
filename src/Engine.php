20aa7e005c9a04009643fdf4dbc392a9bd73c9ca
Alexey Lozhkin <leha.lozhkin.02@mail.ru>
2023-10-06 05:46:40 +0300

    move functions from Info.php to Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -5,6 +5,29 @@ namespace BrainGames\Engine;
 use function cli\line;
 use function cli\prompt;
 
+function welcomePlayer(): string
+{
+    line("Welcome to the Brain Games!");
+    $name = prompt('May I have your name?', '', "\n");
+    line("Hello, %s", $name);
+
+    return $name;
+}
+
+function showRules(string $rules): void
+{
+    line($rules);
+}
+
+function showResult(bool $isWonGame, string $player): void
+{
+    if ($isWonGame) {
+        line('Congratulations, %s!', $player);
+    } else {
+        line("Let's try again, %s!", $player);
+    }
+}
+
 function getRandomNumber(int $max = 0, int $min = 50): int
 {
     return rand($min, $max);
