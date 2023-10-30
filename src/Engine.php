bb056f8726eb92af922953ca831421137ea266c6
Alexey Lozhkin <leha.lozhkin.02@mail.ru>
2023-10-30 10:30:14 +0300

    update Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -9,48 +9,32 @@ const ROUND_COUNT = 3;
 
 function startGame(string $gameRules, callable $getRoundData): void
 {
-    $player = welcomePlayer();
+    line("Welcome to the Brain Games!");
+    $playerName = prompt('May I have your name?', '', "\n");
+    line("Hello, %s", $playerName);
     line($gameRules);
 
-    $gameData = getGameData($getRoundData);
-    $isWonGame = play($gameData);
-
-    showResult($isWonGame, $player);
-}
-
-function welcomePlayer(): string
-{
-    line("Welcome to the Brain Games!");
-    $name = prompt('May I have your name?', '', "\n");
-    line("Hello, %s", $name);
-
-    return $name;
-}
-
-function getGameData(callable $getRoundData): array
-{
     $gameData = [];
 
     for ($i = 0; $i < ROUND_COUNT; $i++) {
         $gameData[] = $getRoundData();
     }
 
-    return $gameData;
-}
-
-function play(array $gameData): bool
-{
     $isWonGame = true;
 
     foreach ($gameData as [$forCreateQuestion, $expectedAnswer]) {
         $isWonGame = startNextRound($forCreateQuestion, $expectedAnswer);
 
         if (! $isWonGame) {
-            return $isWonGame;
+            break;
         }
     }
 
-    return $isWonGame;
+    if ($isWonGame) {
+        line('Congratulations, %s!', $playerName);
+    } else {
+        line("Let's try again, %s!", $playerName);
+    }
 }
 
 function startNextRound(int|array $forCreateQuestion, int|string $expectedAnswer): bool
@@ -58,23 +42,13 @@ function startNextRound(int|array $forCreateQuestion, int|string $expectedAnswer
     $question = is_array($forCreateQuestion) ? implode(' ', $forCreateQuestion) : $forCreateQuestion;
     line("Question: {$question}");
     $answer = prompt('Your answer');
-    $isCorrectAnswer = $expectedAnswer == $answer ? true : false;
+    $isContinueGame = $expectedAnswer == $answer ? true : false;
 
-    if ($isCorrectAnswer) {
+    if (! $isContinueGame) {
         line('Correct!');
     } else {
         line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $expectedAnswer);
     }
 
-    return $isCorrectAnswer;
-}
-
-function showResult(bool $isWonGame, string $player): void
-{
-
-    if ($isWonGame) {
-        line('Congratulations, %s!', $player);
-    } else {
-        line("Let's try again, %s!", $player);
-    }
+    return $isContinueGame;
 }
